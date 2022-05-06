aad3debea98c8babba15f19fc46d3bdcd2452c62
HidehikoKondo <dev@udonko.net>
2022-05-07 00:51:22 +0900

    コードと画像

---
+++ NoMoreShortMovie.js
@@ -0,0 +1,22 @@
+var timeoutId;
+window.addEventListener("scroll", function () {
+    // スクロールを停止して500ms後に終了とする
+    clearTimeout(timeoutId);
+
+    timeoutId = setTimeout(function () {
+        console.log("search!!!!!!!!");
+        var ary = document.getElementsByClassName("c1et5uql");
+
+        const ary2 = Array.prototype.slice.call(ary);
+        ary2.forEach(function (element) {
+            //            console.log(element.innerHTML);
+            if (element.innerHTML == "リールとショート動画") {
+                console.log("発見！！！！！");
+                var parent = element.parentNode.parentNode.parentNode.parentNode.parentNode.parentNode;
+                parent.remove();
+            } else {
+
+            }
+        });
+    }, 500);
+});
