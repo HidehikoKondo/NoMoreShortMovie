433be80ea22993043e08755a2be6b1693b6111a8
Hidehiko Kondo <dev@udonko.net>
2024-12-09 20:36:56 +0900

    リールとショート動画→リールに変更対応

--- NoMoreShortMovie.js
+++ NoMoreShortMovie.js
@@ -26,7 +26,7 @@ window.addEventListener("scroll", function () {
 
         const ary2 = Array.prototype.slice.call(ary);
         ary2.forEach(function (element) {
-            if (element.innerHTML == "リールとショート動画") {
+            if (element.innerHTML == "リール") {
                 console.log("delete ShortMovie!!");
 
                 // n世代上の親を削除
